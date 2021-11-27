68829f5766b9aabfc455629c7e6ecda6405e06fc
Christa Bridges <christa@pebblepad.co.uk>
2021-11-27 15:25:38 +0000

    solar-system Created a separate function that generates the labels for the planets.

--- Solar System/static/scripts/solarSystem.js
+++ Solar System/static/scripts/solarSystem.js
@@ -1,44 +1,53 @@
 class SolarSystem {
     generateSun() {
       const sun = document.createElement("a-sphere");
-      const label = document.createElement("a-text");
       
       sun.setAttribute("src", "#sun");
       sun.setAttribute("position", "-30 1 -10");
       sun.setAttribute("radius", "3");
 
-      label.setAttribute("value", "Sun");
-      label.setAttribute("color", "#FFEE00");
-      label.setAttribute("position", "-30 5 -10")
-      label.setAttribute("scale", "3 3 3");
-
       const aScene = document.querySelector("a-scene");
       aScene.appendChild(sun);
-      aScene.appendChild(label);
     }
 
     generatePlanets(planets) {
       let distance = -20;
+
       for (let planet of planets) {
         console.log(planet)
         const sphere = document.createElement("a-sphere");
-        const label = document.createElement("a-text");
 
         sphere.setAttribute("src", planet.src);
         sphere.setAttribute("position", `${distance} 1 -10`);
         sphere.setAttribute("radius", planet.radius);
         sphere.setAttribute("animation", planet.animation);
+        const aScene = document.querySelector("a-scene");
 
+        aScene.appendChild(sphere);
+        
+        distance += 5;
+      }
+    }
+
+    generateLabels(planets) {
+      const sunLabel = document.createElement("a-text");
+      const aScene = document.querySelector("a-scene");
+      sunLabel.setAttribute("value", "Sun");
+      sunLabel.setAttribute("color", "#FFEE00");
+      sunLabel.setAttribute("position", "-30 5 -10")
+      sunLabel.setAttribute("scale", "3 3 3");
+      aScene.appendChild(sunLabel)
+
+      let distance = -20;
+
+      for (let planet of planets) {
+        const label = document.createElement("a-text");
         label.setAttribute("value", planet.name);
         label.setAttribute("color", "#FFEE00");
         label.setAttribute("position", `${distance} 2.5 -10`);
         label.setAttribute("scale", "3 3 3");
-
-        const aScene = document.querySelector("a-scene");
-
-        aScene.appendChild(sphere);
-        aScene.appendChild(label);
         distance += 5;
+        aScene.appendChild(label);
       }
     }
 }
