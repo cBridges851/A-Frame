a880798017a658499711f4e0a2cd91a7f22a72e0
Christa Bridges <christa@pebblepad.co.uk>
2021-11-27 16:37:03 +0000

    solar-system Added position property onto the planets, instead of using the distance variable, to help with generating the rings.

--- Solar System/static/scripts/solarSystem.js
+++ Solar System/static/scripts/solarSystem.js
@@ -3,29 +3,43 @@ class SolarSystem {
       const sun = document.createElement("a-sphere");
       
       sun.setAttribute("src", "#sun");
-      sun.setAttribute("position", "-30 1 -10");
-      sun.setAttribute("radius", "3");
+      sun.setAttribute("position", "-30 2 -10");
+      sun.setAttribute("radius", "4");
 
       const aScene = document.querySelector("a-scene");
       aScene.appendChild(sun);
     }
 
     generatePlanets(planets) {
-      let distance = -20;
-
       for (let planet of planets) {
         console.log(planet)
         const sphere = document.createElement("a-sphere");
 
         sphere.setAttribute("src", planet.src);
-        sphere.setAttribute("position", `${distance} 1 -10`);
+        sphere.setAttribute("position", planet.position);
         sphere.setAttribute("radius", planet.radius);
         sphere.setAttribute("animation", planet.animation);
         const aScene = document.querySelector("a-scene");
 
         aScene.appendChild(sphere);
-        
-        distance += 5;
+      }
+    }
+
+    generateRings(planets) {
+      const aScene = document.querySelector("a-scene");
+      for (let planet of planets) {
+        for (let ring of planet.rings) {
+          const torus = document.createElement("a-torus");
+          torus.setAttribute("position", ring.position);
+          torus.setAttribute("color", ring.color);
+          torus.setAttribute("segments-tubular", ring.segments_tubular);
+          torus.setAttribute("radius", ring.radius);
+          torus.setAttribute("radius-tubular", ring.radius_tubular);
+          torus.setAttribute("rotation", ring.rotation);
+          torus.setAttribute("scale", ring.scale);
+          aScene.appendChild(torus);
+        }
+
       }
     }
 
@@ -34,19 +48,22 @@ class SolarSystem {
       const aScene = document.querySelector("a-scene");
       sunLabel.setAttribute("value", "Sun");
       sunLabel.setAttribute("color", "#FFEE00");
-      sunLabel.setAttribute("position", "-30 5 -10")
+      sunLabel.setAttribute("position", "-30 7 -10");
       sunLabel.setAttribute("scale", "3 3 3");
       aScene.appendChild(sunLabel)
-
-      let distance = -20;
-
+      
       for (let planet of planets) {
         const label = document.createElement("a-text");
         label.setAttribute("value", planet.name);
         label.setAttribute("color", "#FFEE00");
-        label.setAttribute("position", `${distance} 2.5 -10`);
+        let positionValues = planet.position.split(" ");
+        let newHeight = parseInt(positionValues[1]) + 1.5;
+        positionValues[1] = newHeight.toString();
+        let newPositionValues = positionValues.toString();
+        newPositionValues = newPositionValues.replaceAll(",", " ")
+        console.log(newPositionValues)
+        label.setAttribute("position", newPositionValues);
         label.setAttribute("scale", "3 3 3");
-        distance += 5;
         aScene.appendChild(label);
       }
     }
